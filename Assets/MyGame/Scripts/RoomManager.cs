f500d9831d6e623f2a50b8f8d8f44aa7dc7a684d
Semin Sabanovic <sab.semin@gmail.com>
2024-12-05 08:49:18 +0100

    Lokale Aenderung an RoomManager.cs

--- Assets/MyGame/Scripts/RoomManager.cs
+++ Assets/MyGame/Scripts/RoomManager.cs
@@ -12,22 +12,43 @@ public class RoomManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        for(int row = 0; row < tableLayout.rows; row++)
+        for (int row = 0; row < tableLayout.rows; row++)
         {
-            for(int col = 0; col < tableLayout.columns; col++)
+            for (int col = 0; col < tableLayout.columns; col++)
             {
                 Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
                 GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
                 Debug.Log("x:" + (col * tableLayout.tableSpacing));
-               
-            }
-            
-        } 
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+                //Sessel platzieren
+                Transform pos1 = table.transform.Find("pos1");
+                Transform pos2 = table.transform.Find("pos2");
+
+                if (pos1)
+                {
+                    Debug.Log("pos1 gefunden: " + pos1.position);
+                    Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
+                }
+                else
+                {
+                    Debug.LogWarning("pos1 nicht gefunden!");
+                }
+                
+                if (pos2)
+                {
+                    Debug.Log("pos2 gefunden: " + pos2.position);
+                    Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
+                else
+                {
+                    Debug.LogWarning("pos2 nicht gefunden!");
+                }
+
+
+            }
+        }
+
+        // Update is called once per frame
         
     }
 }
